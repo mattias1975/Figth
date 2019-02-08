cc4139cd9913f993c3cb1a49acec1d7db26825f4
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-02-08 16:23:44 +0100

    figther8feb

--- Figth/Program.cs
+++ Figth/Program.cs
@@ -9,44 +9,63 @@ namespace Figth
         {
             //skapar en spelar med random namn
             InfoGenerator info = new InfoGenerator(DateTime.Now.Millisecond);
-            string namn;
-            string namn1;
-            string whatToDo;
-                 
+            string Name;
+            string Name1;
+            Name1 = info.NextFullName();//slumpar namn på motståndare
+            bool keepalive;
+
             Random rnd = new Random();
-            namn = info.NextFullName();//slumpar namn på motståndare
+
             int strength = rnd.Next(1, 10);//slumpar strength
             int health = rnd.Next(1, 10); //slumpar health
             int damage = rnd.Next(1, 10);//slumpar damage
 
-            Random rnd1 = new Random();
-            namn = info.NextFullName();//slumpar fram moståndar namn från lexicon.charp.infogenerator
-            int strength1 = rnd.Next(1, 6);
+            int strength1 = rnd.Next(1, 10);//slumpar strength
+            int health1 = rnd.Next(1, 10); //slumpar health
+            int damage1 = rnd.Next(1, 10);//slumpar damage
+
+            //du
+
             
-            int damage1 = rnd.Next(1, 10);
 
-            Console.WriteLine("Your name!");
-            namn1 = Console.ReadLine();
+            Console.WriteLine("Write yout name\n");
+            while (health<=0 || health1<=0) ;
+            Name = Console.ReadLine();
+          
+            Console.WriteLine("strengt" + "" + strength);
+            Console.WriteLine("damage" + "" + damage);
+            Console.WriteLine("Health" + "" + health + "\n");
 
-            //player1
-           
-            Console.WriteLine(namn1);
-            Console.WriteLine("Strength" + " " + strength);
-            Console.WriteLine("Health" + " " + health);
-            Console.WriteLine("Damage" + " " + damage);
 
-            Console.WriteLine("What will you do?\nH-Hit a player\nR-retrie");
-            whatToDo = Console.ReadLine();
+            //motståndare
+            Console.WriteLine(Name1);
+            Console.WriteLine("strengt" + strength1);
+            Console.WriteLine("damage" + damage1);
+            Console.WriteLine("Health" + health1);
 
-            //Result
-            Console.WriteLine("your damage is" + " " + damage1+" "+"and"+" "+namn+" "+"is"+" "+damage);
             Console.ReadLine();
+            health = health - damage1;
+            health1 = health1 - damage;
+
+            Console.WriteLine(Name+ "" + "Health is" + health+" "+"and"+""+Name1+""+ "is"+""+health1);
+            Console.ReadLine();
+
+            if (health <= 0)
+                Console.WriteLine(Name + "" + " is dead");
+            else if (health1 <= 0)
+                Console.WriteLine(Name1 + "" + "is dead");
+
+
             
-        
-            }
+
+
+
+
+        }
+
     }
 }
 
 
-   
+
 

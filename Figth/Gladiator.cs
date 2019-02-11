2745c3dbedc8fe1374184b30c37011f42912aef1
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-02-11 16:30:51 +0100

    New Gladiator

---
+++ Figth/Gladiator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lexicon.CSharp.InfoGenerator;
+
+namespace Figth
+{
+    class Gladiator
+    {
+        static InfoGenerator info = new InfoGenerator(DateTime.Now.Millisecond);
+        string name;
+        public string Name { get { return name; } private set { name = value; } }
+        int strength;
+        int health;
+        int damage;
+        public int Health { get { return health; }
+            set {
+                if (value <= 0)
+                {
+                    Alive = false;
+                }
+                health = value;
+            }
+        }
+        public bool Alive { get; private set; }
+
+        public Gladiator()
+        {
+            name = info.NextFullName();//slumpar namn på motståndare
+            strength = info.Next(1, 10);//slumpar strength
+            health = info.Next(1, 10); //slumpar health
+            damage = info.Next(1, 10);//slumpar damage
+            Alive = true;
+        }
+
+        public Gladiator(string name) : this()
+        {
+            this.name = name;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Name" + "" + name);
+            Console.WriteLine("strengt" + "" + strength);
+            Console.WriteLine("damage" + "" + damage);
+            Console.WriteLine("Health" + "" + health + "\n");
+        }
+    }
+}
--- Figth/Program.cs
+++ Figth/Program.cs
@@ -1,69 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
 using Lexicon.CSharp.InfoGenerator;
 
 namespace Figth
 {
     class Program
     {
+        public static int list { get; private set; }
+
         static void Main(string[] args)
         {
-            //skapar en spelar med random namn
-            InfoGenerator info = new InfoGenerator(DateTime.Now.Millisecond);
+
             string Name;
-            string Name1;
-            Name1 = info.NextFullName();//slumpar namn på motståndare
+            string newgame;
             bool keepalive;
 
-            Random rnd = new Random();
 
-            int strength = rnd.Next(1, 10);//slumpar strength
-            int health = rnd.Next(1, 10); //slumpar health
-            int damage = rnd.Next(1, 10);//slumpar damage
-
-            int strength1 = rnd.Next(1, 10);//slumpar strength
-            int health1 = rnd.Next(1, 10); //slumpar health
-            int damage1 = rnd.Next(1, 10);//slumpar damage
 
             //du
+            Console.WriteLine("Write your name\n");
 
-            
-
-            Console.WriteLine("Write yout name\n");
-            while (health<=0 || health1<=0) ;
             Name = Console.ReadLine();
-          
-            Console.WriteLine("strengt" + "" + strength);
-            Console.WriteLine("damage" + "" + damage);
-            Console.WriteLine("Health" + "" + health + "\n");
-            
+            Gladiator me = new Gladiator(Name);
+
             //motståndare
-            Console.WriteLine(Name1);
-            Console.WriteLine("strengt" + strength1);
-            Console.WriteLine("damage" + damage1);
-            Console.WriteLine("Health" + health1);
-
-            Console.ReadLine();
-            health = health - damage1;
-            health1 = health1 - damage;
-
-            Console.WriteLine(Name+ "" + "Health is" + health+" "+"and"+""+Name1+""+ "is"+""+health1);
-            Console.ReadLine();
-
-            if (health <= 0)
-                Console.WriteLine(Name + "" + " is dead");
-            else if (health1 <= 0)
-                Console.WriteLine(Name1 + "" + "is dead");
+            Gladiator enamy = new Gladiator();
 
 
-            
+            keepalive = true;
+            while (keepalive)
+
+            {
+                if (me.Alive && enamy.Alive)
+                {
+                    me.Health--;
+                    enamy.Health--;
+                    Console.WriteLine(me.Name + " " + "health is" + " " + me.Health + " " + enamy.Name + " helath is" + " " + enamy.Health);
+
+                    Console.ReadKey();
+                }
+                else if (!me.Alive)
+                {
+
+                    Console.WriteLine(me.Name + "" + "You are dead");
+                 
+                    Console.ReadKey();
+                    foreach(string streangth in Gladiator())
+                        Console.Write($"{streangth});
+                    keepalive = false;
+                }
+                else if (!enamy.Alive)
+                {
+
+                    Console.WriteLine(enamy.Name + "" + " is dead will you play again? y/n?");
+                    newgame = Console.ReadLine();
+                    if (newgame == "Y")
+                        enamy = new Gladiator();
+                    else
+                        keepalive = false;
+
+                }
 
 
-
+            }
 
         }
 
+        private static IEnumerable<object> Gladiator()
+        {
+            throw new NotImplementedException();
+        }
     }
+
 }
+
+
+
 
 
 
